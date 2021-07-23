3b15c0aeb9df5ac349fb7b102cddfb4ecdea4b42
Arcadeena <mattmcc97@gmail.com>
2021-07-24 00:03:12 +0930

    Update EmbedFactory.ts

--- src/interactions/factories/EmbedFactory.ts
+++ src/interactions/factories/EmbedFactory.ts
@@ -1,10 +1,24 @@
-import { MessageEmbed, MessageEmbedOptions, InteractionReplyOptions } from 'discord.js'
+import { MessageEmbed, MessageEmbedOptions, InteractionReplyOptions, Message, GuildMember } from 'discord.js'
 import { BaseFactory, BaseHandler, HandlerContext } from '../..';
 
 export class EmbedFactory<H extends BaseHandler> extends BaseFactory<H> {
 
     constructor(handler: H) {
         super(handler);
+    }
+
+    public getEmbedTemplate(context: HandlerContext, data?: MessageEmbed | MessageEmbedOptions): HandlerEmbed {
+        const embed = new HandlerEmbed(context, data);
+        const { member } = context;
+        const user = context instanceof Message ? context.author : context.user;
+        if (member && member instanceof GuildMember) {
+            embed.setAuthor(member.displayName, user.displayAvatarURL());
+            embed.setColor(member.displayColor || 14840969);
+        } else {
+            embed.setAuthor(user.username, user.displayAvatarURL());
+            embed.setColor(14840969);
+        }
+        return embed;
     }
 }
 
