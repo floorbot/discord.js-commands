30fe8e0c1a6aa6a308271f89199102f2fbc3c57e
Arcadeena <mattmcc97@gmail.com>
2021-07-23 20:28:12 +0930

    Update CommandHandler.ts

--- src/interactions/handlers/CommandHandler.ts
+++ src/interactions/handlers/CommandHandler.ts
@@ -1,10 +1,10 @@
 import { CommandInteraction, ApplicationCommand, ApplicationCommandData, Guild } from 'discord.js';
-import { BaseHandler, HandlerResult } from '../..';
+import { CommandClient, BaseHandler, HandlerResult } from '../..';
 
 export interface CommandHandler extends BaseHandler {
 
     readonly commandData: ApplicationCommandData;
 
     onCommand(interaction: CommandInteraction): Promise<HandlerResult | null | any>;
-    fetchCommand(guild?: Guild): Promise<ApplicationCommand | null>;
+    fetchCommand(guild: Guild | CommandClient): Promise<ApplicationCommand | null>;
 }
