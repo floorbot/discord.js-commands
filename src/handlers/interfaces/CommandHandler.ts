9d670e543810f91cd27b2546d9b08dedfbfc07de
Arcadeena <mattmcc97@gmail.com>
2021-11-06 18:31:51 +1030

    fixed?

--- src/handlers/interfaces/CommandHandler.ts
+++ src/handlers/interfaces/CommandHandler.ts
@@ -4,6 +4,7 @@ import { CommandClient, BaseHandler, HandlerResult } from '../..';
 export interface CommandHandler extends BaseHandler {
 
     readonly commandData: ApplicationCommandData;
+    readonly isGlobal: boolean;
 
     onCommand(interaction: BaseCommandInteraction): Promise<HandlerResult | null | any>;
     fetchCommand(guild: Guild | CommandClient): Promise<ApplicationCommand | null>;
